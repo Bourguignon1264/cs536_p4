a3f76ecbac4adc58e961ff1a10338fd052a8ec07
Bourguignon1264 <bourguignon1264@gmail.com>
2022-04-10 12:18:15 -0500

    ErrMsg

--- files/ErrMsg.java
+++ files/ErrMsg.java
@@ -4,6 +4,9 @@
  * This class is used to generate warning and fatal error messages.
  */
 class ErrMsg {
+
+    static boolean isFatal = false;
+
     /**
      * Generates a fatal error message.
      * @param lineNum line number for error location
@@ -11,6 +14,7 @@ class ErrMsg {
      * @param msg associated message for error
      */
     static void fatal(int lineNum, int charNum, String msg) {
+        isFatal = true;
         System.err.println(lineNum + ":" + charNum + " ****ERROR**** " + msg);
     }
 
